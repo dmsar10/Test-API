abe38ff2de38e60bb2a258a9bed099f3a6cca98f
Dimas Arya <dimasarya96@gmail.com>
2023-01-25 22:07:40 +0700

    Update DeleteBooking Testing

--- Object Repository/Booking API/DeleteBooking.rs
+++ Object Repository/Booking API/DeleteBooking.rs
@@ -11,11 +11,35 @@
    <httpBody></httpBody>
    <httpBodyContent></httpBodyContent>
    <httpBodyType></httpBodyType>
+   <httpHeaderProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>Cookie</name>
+      <type>Main</type>
+      <value>token=&lt;token_value></value>
+      <webElementGuid>dbb496c9-a3cf-49e3-9a81-60c5c09326e0</webElementGuid>
+   </httpHeaderProperties>
+   <httpHeaderProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>Authorization</name>
+      <type>Main</type>
+      <value>Basic YWRtaW46cGFzc3dvcmQxMjM=</value>
+      <webElementGuid>b503a206-42ca-4b64-aba4-3d52fba56c2c</webElementGuid>
+   </httpHeaderProperties>
+   <httpHeaderProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>Content-Type</name>
+      <type>Main</type>
+      <value>application/json</value>
+      <webElementGuid>d96afb3c-e9e4-421a-ba2e-9296c6797986</webElementGuid>
+   </httpHeaderProperties>
    <katalonVersion>8.5.5</katalonVersion>
    <maxResponseSize>0</maxResponseSize>
    <migratedVersion>5.4.1</migratedVersion>
    <restRequestMethod>DELETE</restRequestMethod>
-   <restUrl>https://restful-booker.herokuapp.com/booking/10212</restUrl>
+   <restUrl>https://restful-booker.herokuapp.com/booking/1</restUrl>
    <serviceType>RESTful</serviceType>
    <soapBody></soapBody>
    <soapHeader></soapHeader>
